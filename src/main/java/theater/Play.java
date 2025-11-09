67c38397480cd28a9872b64d8ba111b70b5e1be7
Abdullah <abdullah.algothami@mail.utoronto.ca>
2025-11-09 07:24:46 +0300

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,31 @@
 package theater;
 
+/**
+ * Represents a play in theater with a name and a type.
+ * This class holds information about the play like its name and type.
+ */
 public class Play {
+    // make it private
+    private String name;
+    private String type;
 
-    public String name;
-    public String type;
-
+    /**
+     * Constructs a new {@code Play} with a specified name and type.
+     *
+     * @param name the name of the play.
+     * @param type the type of the play.
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
     }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
